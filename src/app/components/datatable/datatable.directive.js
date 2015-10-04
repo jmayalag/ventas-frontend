9e53e26b52bef7e8ea23e7c3a014b1095ec37d35
Jordan <jayala1993@outlook.com>
2015-10-04 17:32:46 -0300

    Table with pagination

--- src/app/components/datatable/datatable.directive.js
+++ src/app/components/datatable/datatable.directive.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -11,7 +11,8 @@
       restrict: 'E',
       templateUrl: 'app/components/datatable/datatable.html',
       scope: {
-          serviceName: '@'
+        options: '=',
+        serviceName: '@'
       },
       controller: DataTableController,
       controllerAs: 'vm',
@@ -24,7 +25,55 @@
     function DataTableController($injector) {
       var vm = this;
       var service = $injector.get(vm.serviceName);
-      vm.data = service.get();
+      var o = vm.options;
+
+      var pageRestart = function () {
+        vm.current_page = 1;
+      }
+
+      var getData = function () {
+        vm.queryParams.page = vm.current_page;
+        vm.loading = true;
+        var data = service.get(vm.queryParams, function () {
+          vm.loading = false;
+          vm.items = data[o.data_prop];
+          vm.pages = data.meta.total_pages;
+          console.log("Datos obtenidos");
+        }, function () {
+          alert("No se pudo obtener datos del servidor.");
+          console.log("Fallo en obtener datos");
+        });
+      };
+
+      vm.getPages = function () {
+        var pages = new Array(vm.pages);
+        return pages;
+      };
+
+      vm.prevPage = function () {
+        console.log("prev");
+        if (vm.current_page > 1)
+          vm.changePage(vm.current_page - 1);
+      };
+
+      vm.nextPage = function () {
+        if (vm.current_page < vm.pages)
+          vm.changePage(vm.current_page + 1);
+      };
+
+      vm.changePage = function (newPage) {
+        if (newPage == vm.current_page) return;
+        vm.current_page = newPage;
+        getData();
+      };
+
+      var init = function () {
+        vm.queryParams = {};
+        pageRestart();
+        getData();
+      };
+
+      init();
     }
   }
 
--- src/app/proveedor/proveedor.controller.js
+++ src/app/proveedor/proveedor.controller.js
@@ -9,6 +9,26 @@
     //  {id: 1, nombre: 'Jordan', ci: '4058908'},
     //  {id: 2, nombre: 'Alma', ci: '4058908'}
     //];
-    $scope.proveedores = Proveedor.get();
+    //$scope.proveedores = Proveedor.get();
+
+    $scope.options = {
+      column_defs: {
+        id: 'ID',
+        nombre: 'Nombre',
+        ruc: 'RUC',
+        email: 'Email'
+      },
+      data_prop: 'listado'
+    };
+
+    $scope.crear = function () {
+      var proveedor = new Proveedor();
+      proveedor['nombre'] = 'Prueba';
+      proveedor['ruc'] = '3090';
+      proveedor['email'] = 'email@son.com';
+      proveedor['telefono'] = '49049';
+      console.log(proveedor);
+      Proveedor.save(proveedor);
+    };
   }
 })();
