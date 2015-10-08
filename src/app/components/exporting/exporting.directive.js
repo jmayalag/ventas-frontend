13d171854cf680aae16538a91166e74df7e119e6
Jordan <jayala1993@outlook.com>
2015-10-08 15:39:06 -0300

    Export directive

---
+++ src/app/components/exporting/exporting.directive.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ventas')
+    .directive('exporting', exporting);
+
+  /** @ngInject */
+  function exporting() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/exporting/exporting.html',
+      scope: {
+        options: '='
+      },
+      controller: ExportController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function ExportController($log, $location, $mdDialog) {
+      var vm = this;
+      var o = vm.options;
+
+      $log.info("Exportando");
+      vm.mensaje = 'exportando';
+    }
+  }
+
+})
+();
