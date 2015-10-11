fbc5ba24f5286ed29a51a73624467462c363fc51
Jordan <jayala1993@outlook.com>
2015-10-11 20:20:26 -0300

    Batch directive

---
+++ src/app/components/batch/batch.directive.js
@@ -0,0 +1,88 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ventas')
+    .directive('customForm', customForm);
+
+  /** @ngInject */
+  function customForm() {
+    var directive = {
+      restrict: 'E',
+      transclude: true,
+      templateUrl: 'app/components/customForm/customForm.html',
+      scope: {
+        options: '=',
+        item: '='
+      },
+      controller: CustomFormController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function CustomFormController($log, $location) {
+      var vm = this;
+      var o = vm.options;
+      var successUrl = function (id) {
+        return o.successPrefix + id;
+      };
+
+      vm.title = o.title;
+      vm.fields = o.fields;
+      vm.previous = o.previous;
+
+      if (!vm.item) {
+        vm.item = {};
+      }
+
+      vm.cancel = o.cancel ? o.cancel : function () {
+        $location.path(vm.previous).replace(); //sin callback
+      };
+
+      if (o.saveItem) {
+        vm.persist = function () { //callback
+          o.saveItem(vm.object);
+        };
+      } else { //sin callback
+        if (!o.edit) { //si es creacion
+          vm.persist = function () {
+            vm.item.$save(function (data) {
+              $log.info("Guardado");
+              $location.path(successUrl(data.id));
+            }, function (err) {
+              $mdDialog.show(
+                $mdDialog.alert()
+                  .clickOutsideToClose(true)
+                  .title('No se pudo guardar')
+                  .content(err)
+                  .ok('Ok')
+              );
+              $log.error("Error al guardar");
+            });
+          };
+        } else {
+          vm.persist = function () {
+            vm.item.$update(function (data) {
+              $log.info("Updated");
+              $location.path(successUrl(data.id));
+            }, function (err) {
+              $mdDialog.show(
+                $mdDialog.alert()
+                  .clickOutsideToClose(true)
+                  .title('No se pudo actualizar')
+                  .content(err)
+                  .ok('Ok')
+              );
+              $log.error("Error update");
+            });
+          };
+        }
+      }
+    }
+  }
+
+})
+();
