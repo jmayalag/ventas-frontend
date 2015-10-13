5cc0fd3d2796ee37ac3ae3d901e8cd2c7926e2ef
Jordan <jayala1993@outlook.com>
2015-10-13 12:10:05 -0300

    Use ng-upload

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('ventas', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ngMaterial']);
+    .module('ventas', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ngMaterial', 'ngFileUpload']);
 
 })();
