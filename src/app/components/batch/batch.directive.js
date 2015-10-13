54119cfd6f397101d79e5156ca9b85b5688680b3
Jordan <jayala1993@outlook.com>
2015-10-13 12:09:17 -0300

    Add uploadUrl

--- src/app/components/batch/batch.directive.js
+++ src/app/components/batch/batch.directive.js
@@ -23,7 +23,7 @@
     /** @ngInject */
     function BatchController($log, $mdDialog, $document) {
       var vm = this;
-      var service = vm.options.service;
+      var uploadUrl = vm.options.uploadUrl;
 
       vm.showDialog = function (ev) {
         $mdDialog.show({
@@ -35,7 +35,8 @@
         });
       };
 
-      function DialogController($scope, $mdDialog) {
+      /** @ngInject */
+      function DialogController($scope, $mdDialog, Upload) {
         $scope.selected = false;
 
         $scope.cancel = function () {
@@ -44,19 +45,33 @@
         };
 
         $scope.upload = function (file) {
-          if (file) {
-            $scope.selected = true;
-            $scope.loading = true;
-            $log.info(file);
-            service.batch(file, function (data) {
-              $scope.loading = false;
-              $scope.result = data.message;
-            }, function (err) {
-              $log.error(err);
-            });
-          }
+          $scope.selected = true;
+          $scope.loading = true;
+          console.log(vm.options);
+          Upload.upload({
+            url: uploadUrl,
+            data: {file: file}
+          }).then(function (resp) {
+            $scope.loading = false;
+            $scope.result = resp.data.message;
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+          }, function (resp) {
+            $scope.loading = false;
+            $scope.result = resp.statusText;
+            console.log(resp);
+            console.log('Error status: ' + resp.status);
+          }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+          });
         };
 
+
+        $scope.submit = function () {
+          if ($scope.file && !$scope.file.$error) {
+            $scope.upload($scope.file);
+          }
+        };
       }
     }
   }
